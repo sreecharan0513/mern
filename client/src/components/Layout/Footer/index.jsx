274dc71f70cf279958c7618087653353a50146a0
sreecharan0513 <139733279+sreecharan0513@users.noreply.github.com>
2023-11-22 08:52:58 +0530

    Add files via upload

---
+++ client/src/components/Layout/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+const Footer = () => {
+  return (
+    <footer className="general_footer">
+      <span className="copyright">Pets Reunite Hub, {new Date().getFullYear()}. ALL RIGHTS RECEIVED</span>
+      
+    </footer>
+  )
+}
+
+export default Footer;
